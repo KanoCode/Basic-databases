cc6ecf66a5c1f424327c8acc89db5221cc448a82
KanoCode <kanokanombola@gmail.com>
2022-07-08 23:59:18 +0200

    Create database schemas

--- schema.sql
+++ schema.sql
@@ -1,10 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
--- CREATE TABLE animals(
--- id INT GENERATED ALWAYS AS IDENTITY,name TEXT NOT NULL, 
--- date_of_birth DATE NOT NULL,
--- escape_attempts INT, neutered TEXT NOT NULL, 
--- weight_kg FLOAT NOT NULL);
 CREATE TABLE animals(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 name VARCHAR(50),
 date_of_birth DATE NOT NULL,escape_attempts INT,neutered VARCHAR(30),
