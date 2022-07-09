0e7cffbac2c45104ae763047f40eda9f6be3e89b
KanoCode <kanokanombola@gmail.com>
2022-07-09 21:44:23 +0200

    Create new tables, remove species column

--- schema.sql
+++ schema.sql
@@ -9,3 +9,31 @@ weight_kg FLOAT);
 -- Added a new column to the animals table;
 ALTER TABLE animals
  ADD species TEXT; species TEXT
+
+ -- Add new tables inside vet_clinic
+CREATE TABLE owners(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+full_name VARCHAR(50),
+age INT);
+
+-- Create a table named species
+
+CREATE TABLE species(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+name VARCHAR(50));
+
+-- Remove column species
+ALTER TABLE animals
+  DROP COLUMN species;
+
+  --ADD new column species_id which is a foreign key which references the species table
+  ALTER TABLE animals
+  ADD COLUMN species_id INT;
+
+  ALTER TABLE animals
+    ADD FOREIGN KEY (species_id) REFERENCES species(id);
+  -- ADD new column owner_id which is a foreign key referencing owners table
+
+   ALTER TABLE animals
+  ADD COLUMN owner_id INT;
+
+    ALTER TABLE animals
+    ADD FOREIGN KEY (owner_id) REFERENCES owner(id);
