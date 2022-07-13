0b6c4866422c0fbb86929740feaafee4f0fe72b4
SimbaWANGU <simbawangulu@gmail.com>
2022-07-13 17:19:49 +0300

    Add sql for erd schema

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,3 +4,31 @@ CREATE TABLE patients( id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR, date_o
  CREATE INDEX patient_index ON medical_histories(patient_id ASC); 
  CREATE TABLE invoices ( invoice_id INT GENERATED ALWAYS AS IDENTITY, total_amount FLOAT, generated_at TIMESTAMP, payed_at TIMESTAMP, medical_history_id INT REFERENCES medical_histories(history_id) PRIMARY KEY(invoice_id) ); 
  CREATE INDEX invoice_index ON invoices(medical_history_id ASC); 
+
+ CREATE TABLE invoice_items (
+	invoice_item_id INT GENERATED ALWAYS AS IDENTITY,
+	unit_price FLOAT,
+	quantity INT,
+	total_price FLOAT,
+	invoice_id INT REFERENCES invoices(invoice_id),
+	treatment_id INT REFERENCES treatments(treatment_id),
+	PRIMARY KEY(invoice_item_id)
+);
+
+CREATE INDEX invoice_items ON invoices_items(invoice_id ASC);
+
+CREATE INDEX invoice_items ON invoices_items(treatment_id ASC);
+
+CREATE TABLE treatments (
+	treatment_id INT GENERATED ALWAYS AS IDENTITY,
+	type VARCHAR,
+	name VARCHAR,
+	PRIMARY KEY(treatment_id)
+);
+
+CREATE TABLE history_treatment (
+	histories_id INT REFERENCES medical_histories (histories_id),
+	treatment_id INT REFERENCES treatments (treatment_id)
+);
+
+CREATE INDEX histories_id ON history_treatment(histories_id ASC);
